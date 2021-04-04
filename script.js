2946037530e5c6dca2d30ccc41a763aba771e300
Omri <omri.ronen4@gmail.com>
2021-04-03 18:42:19 -0700

    fixed some bugs where the values from the arrays were not showing up. On to the final stage
    git status

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ function generatePassword () {
    var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
    var numberCase = [0,1,2,3,4,5,6,7,8,9];
    var specialCase = ["!","@","#","$","%","^","&","*","(",")","_","-","+","=","|","}","]","{","[",'"',"'",":",";","?","/",">",".","<",",","~","`"];
-   var combinationCase = []
+   var combinationCase = [];
 
    var passwordAnswer = window.prompt("How long do you want your password to be?") 
    if (passwordAnswer >= 8 && passwordAnswer <= 128) {
+       console.log("password length",passwordAnswer)
        var upper = confirm("Do you want to use upper case letters?");
        var lower = confirm("Do you want to use lower case letters?");
        var number = confirm("Do you want to use numbers?");
@@ -18,19 +19,36 @@ function generatePassword () {
         console.log(lower);
         console.log(number);
         console.log(special);
+        
+        // var index = Math.floor(Math.random()*passwordAnswer)   
+        var indexUpper =  upperCase[Math.floor(Math.random() * upperCase.length)];
+        console.log(indexUpper);
+        var indexLower =  lowerCase[Math.floor(Math.random() * lowerCase.length)];
+        console.log(indexLower);
+        var indexNumber=  numberCase[Math.floor(Math.random() * numberCase.length)];
+        console.log(indexNumber);
+        var indexSpecial =  specialCase[Math.floor(Math.random() * specialCase.length)];
+        console.log(indexSpecial);
+        for (var i=0; i<passwordAnswer;i++) {
+                if (upper) {
+                    combinationCase.push(indexUpper);
+                } 
+                if (lower) {
+                    combinationCase.push(indexLower);
+                }
+                if (number) {
+                    combinationCase.push(indexNumber);
+                }
+                if (special) {
+                    combinationCase.push(indexSpecial);
 
-        if (upper) {
-            combinationCase.push(upperCase);
-        } 
-        if (lower) {
-            combinationCase.push(lowerCase);
-        }
-        if (number) {
-            combinationCase.push(numberCase);
-        }
-        if (special) {
-            combinationCase.push(specialCase)
-        }
+                }
+                window.prompt(combinationCase)
+            }
+
+        
+        
+        console.log(combinationCase)
 
    } else {
        alert("Error! Does not meet password criteria. Must be between 8 and 128 characters.");
@@ -42,6 +60,7 @@ function generatePassword () {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
 
   passwordText.value = password;
 
@@ -50,3 +69,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
