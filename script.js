d5f611e6030e0f68961a342c15a271c8739bc63a
Omri <omri.ronen4@gmail.com>
2021-04-03 21:58:50 -0700

    finished the password generator and fixed all the bugs

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword () {
-   var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-   var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-   var numberCase = [0,1,2,3,4,5,6,7,8,9];
-   var specialCase = ["!","@","#","$","%","^","&","*","(",")","_","-","+","=","|","}","]","{","[",'"',"'",":",";","?","/",">",".","<",",","~","`"];
-   var combinationCase = [];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numberCase = [0,1,2,3,4,5,6,7,8,9];
+var specialCase = ["!","@","#","$","%","^","&","*","(",")","_","-","+","=","|","}","]","{","[",'"',"'",":",";","?","/",">",".","<",",","~","`"];
+var combinationCase = [];
 
+function generatePassword () {
+  
    var passwordAnswer = window.prompt("How long do you want your password to be?") 
    if (passwordAnswer >= 8 && passwordAnswer <= 128) {
        console.log("password length",passwordAnswer)
@@ -21,34 +22,44 @@ function generatePassword () {
         console.log(special);
         
         // var index = Math.floor(Math.random()*passwordAnswer)   
-        var indexUpper =  upperCase[Math.floor(Math.random() * upperCase.length)];
-        console.log(indexUpper);
-        var indexLower =  lowerCase[Math.floor(Math.random() * lowerCase.length)];
-        console.log(indexLower);
-        var indexNumber=  numberCase[Math.floor(Math.random() * numberCase.length)];
-        console.log(indexNumber);
-        var indexSpecial =  specialCase[Math.floor(Math.random() * specialCase.length)];
-        console.log(indexSpecial);
-        for (var i=0; i<passwordAnswer;i++) {
+        
+        for (var i=0; i<passwordAnswer;) {
                 if (upper) {
-                    combinationCase.push(indexUpper);
-                } 
+                    if (i>=passwordAnswer){
+                        return;
+                    }
+                    var indexUpper =  upperCase[Math.floor(Math.random() * upperCase.length)];
+                     combinationCase.push(indexUpper);
+                     i++;
+                     console.log(combinationCase)
+                     
+                }           
                 if (lower) {
+                    if (i>=passwordAnswer){
+                        return;}
+                    var indexLower =  lowerCase[Math.floor(Math.random() * lowerCase.length)];
                     combinationCase.push(indexLower);
+                    i++;
+                    console.log(combinationCase)
                 }
                 if (number) {
+                    if (i>=passwordAnswer){
+                        return;}
+                    var indexNumber =  numberCase[Math.floor(Math.random() * numberCase.length)];
                     combinationCase.push(indexNumber);
+                    i++;
+                    console.log(combinationCase)
                 }
                 if (special) {
+                    if (i>=passwordAnswer){
+                        return;}
+                    var indexSpecial =  specialCase[Math.floor(Math.random() * specialCase.length)];
                     combinationCase.push(indexSpecial);
-
+                    i++;
+                    console.log(combinationCase)
                 }
-                window.prompt(combinationCase)
+                
             }
-
-        
-        
-        console.log(combinationCase)
 
    } else {
        alert("Error! Does not meet password criteria. Must be between 8 and 128 characters.");
@@ -60,13 +71,11 @@ function generatePassword () {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
-
-  passwordText.value = password;
-
+  passwordText.value = combinationCase;
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
 
