694b72f71cdfa58a0907b3e1d67a5cefd5c34dfa
Omri <omri.ronen4@gmail.com>
2021-04-05 12:02:50 -0700

    made some changes to the comments

--- script.js
+++ script.js
@@ -6,11 +6,10 @@ var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numberCase = [0,1,2,3,4,5,6,7,8,9];
 var specialCase = ["!","@","#","$","%","^","&","*","(",")","_","-","+","=","|","}","]","{","[",'"',"'",":",";","?","/",">",".","<",",","~","`"];
-// This array will include the new password that we will generate at the end.
-var combinationCase = [];
+var combinationCase = []; // This array will include the new password that we will generate at the end.
 
-//This function will take in data from the window prompt, and also take in data from 4 confirm windows. 
-//This function will also use a for loop to generate random variables from our arrays and insert them into the combinationCase array
+//The generatePassword function will take in data from the window prompt, and also take in data from 4 confirm windows. 
+//The generatePassword function will also use a for loop to generate random variables from our arrays and insert them into the combinationCase array
 function generatePassword () {
    var passwordAnswer = window.prompt("How long do you want your password to be?") 
    if (passwordAnswer >= 8 && passwordAnswer <= 128) {
@@ -23,29 +22,24 @@ function generatePassword () {
         console.log(lower);
         console.log(number);
         console.log(special);
-        // this will clear the array from previous password generation attempts.
-        combinationCase = [];
+        combinationCase = []; // this will clear the array from previous password generation attempts. 
         
-        // var index = Math.floor(Math.random()*passwordAnswer)   
-        
-        for (var i=0; i<passwordAnswer;) {
-            //if the confirm method is true for upper, we will continue with the loop.
-                if (upper) {
-                    //if the iteration is bigger than or equal than the password length desired, the for loop will return.
-                    if (i>=passwordAnswer){
+        for (var i=0; i<passwordAnswer;) {    // Here we start the loop to generate a password
+           
+                if (upper) {   //if the confirm method is true for upper, we will continue with the loop.
+                    
+                    if (i>=passwordAnswer){ //if the iteration is bigger than or equal than the password length desired, the for loop will return.
                         return;
                     }
-                    //Here we generate a random index from the upperCase array which gets us an upper case value, and we push it into the combination array.
+                    //Here we generate a random number which serves as the random index from the upperCase array. The random upper case value that was chosen will be pushed it into the combination array.
                     var indexUpper =  upperCase[Math.floor(Math.random() * upperCase.length)];
                      combinationCase.push(indexUpper);
-                     //Here we want to add an iteration 
-                     i++;
-                     //Here we log our information into combination case.
-                     console.log(combinationCase)
+                     i++; //Here we want to add an iteration to the loop
+                     console.log(combinationCase)  //Here we log our information into combination case.
                      
-                     // The rest of the if stements match the pattern described above/
+              // The rest of the if stements below match the pattern of the if statement described above^.
                 }           
-                if (lower) {
+                if (lower) { 
                     if (i>=passwordAnswer){
                         return;}
                     var indexLower =  lowerCase[Math.floor(Math.random() * lowerCase.length)];
@@ -79,12 +73,12 @@ function generatePassword () {
 }
 
 function writePassword() {
-  generatePassword();                                       // here we are calling the generatePasword function.
-  var passwordText = document.querySelector("#password");    // This replaces the password area in the html and assigns it a new value
-  passwordText.value = combinationCase.join(('')); // the passwordText value gets reassigned to equal the combinationCase array, so the password can be displayed nicely in the middle.
-}
+  generatePassword();    // here we are calling the generatePasword function.
+  var passwordText = document.querySelector("#password"); // Here we make a variable for the password conainer in the html.
+  passwordText.value = combinationCase.join(('')); // the value inside the passwordText container will equal the combinationCase array, so the made password from the combinationCase array will be displayed in the passwordText container.
+} 
 
-generateBtn.addEventListener("click", writePassword); // this adds an event listener of click to the generateBtn. When the button is clicked, our writePassword function runs, and the entire password generator begins its process. 
+generateBtn.addEventListener("click", writePassword); // this adds an event listener of click to the generate password button. When the button is clicked, our writePassword function runs, and the entire password generator begins its process starting with the generatePassword function being called.
 
 
 
